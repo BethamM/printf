4a63d5ffc8dbb246970a5dc4566713b04e7eedc2
kimaita <johnnieqaym@gmail.com>
2023-01-26 19:57:29 +0000

    updates

--- _printf.c
+++ _printf.c
@@ -3,7 +3,13 @@
 #include <string.h>
 #include <unistd.h>
 #include <stddef.h>
-#include <stdio.h>
+
+/**
+ * _printf - function that produces output according to a format
+ * @format: format specifier
+ *
+ * Return: number of characters printed
+ */
 int _printf(const char *format, ...)
 {
 	unsigned int len, i;
@@ -13,21 +19,18 @@ int _printf(const char *format, ...)
 
 	len = strlen(format);
 	va_start(args, format);
+	characters = 0;
 
 	for (i = 0; i < len; i++)
 	{
 		c = format + i;
-		if (*c == '%')
+		if (*c == '%' && get_conversion(c + 1) != NULL)
 		{
- 			if (get_conversion(c + 1) != NULL)
- 			{
-				characters = get_conversion(c + 1)(args);
-				i++;
- 			}
+			characters += get_conversion(c + 1)(args);
+			i++;
 		}
 		else
-			characters = write(1, c, 1);
-
+			characters += write(1, c, 1);
 	}
 	va_end(args);
 	return (characters);
